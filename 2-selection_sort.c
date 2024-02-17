c52a29cb69bf38025b4eb6aae16e38f140d89f5c
Yassid-tech <yassine.idrissi2-etu@etu.univh2c.ma>
2024-02-17 13:34:12 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -23,10 +23,10 @@ tmpo = *a;
 void selection_sort(int *array, size_t size)
 {
 int *min;
-size_t i,j;
+size_t i, j;
 if (array == NULL || size < 2)
 return;
-for (i=0; i < size - 1; i++)
+for (i = 0; i < size - 1; i++)
 {
 min = array + i;
 for (j = i + 1; j < size; j++)
